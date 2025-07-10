75f56bef1726594c2cf7b90594ec3fba8dfbaf71
kennataddese6 <kennataddese6@gmail.com>
2025-07-10 16:15:36 +0300

    changes types in columns

--- app/ma/leads/columns.tsx
+++ app/ma/leads/columns.tsx
@@ -4,24 +4,19 @@ import { ColumnDef } from "@tanstack/react-table"
 
 // This type is used to define the shape of our data.
 // You can use a Zod schema here if you want.
-export type Payment = {
-  id: string
-  amount: number
-  status: "pending" | "processing" | "success" | "failed"
+export type Lead = {
+  id: number
   email: string
+  description: string
 }
 
-export const columns: ColumnDef<Payment>[] = [
-  {
-    accessorKey: "status",
-    header: "Status",
-  },
+export const columns: ColumnDef<Lead>[] = [
   {
     accessorKey: "email",
     header: "Email",
   },
   {
-    accessorKey: "amount",
-    header: "Amount",
+    accessorKey: "description",
+    header: "Description",
   },
 ]
