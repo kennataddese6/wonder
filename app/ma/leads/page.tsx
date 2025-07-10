bbc794d7d124bb34ac7a6a4decb40998e4953d0d
kennataddese6 <kennataddese6@gmail.com>
2025-07-10 16:16:56 +0300

    use create lead function

--- app/ma/leads/page.tsx
+++ app/ma/leads/page.tsx
@@ -1,23 +1,10 @@
+import { getLeads } from "@/action/action"
 import { CreateLead } from "@/components/molecules/create-lead"
-import { columns, Payment } from "./columns"
+import { columns } from "./columns"
 import { DataTable } from "./data-table"
 
-async function getData(): Promise<Payment[]> {
-  // Fetch data from your API here.
-  return [
-    {
-      id: "728ed52f",
-      amount: 100,
-      status: "pending",
-      email: "m@example.com",
-    },
-    // ...
-  ]
-}
-
 export default async function Page() {
-  const data = await getData()
-
+  const data = await getLeads()
   return (
     <div className="container mx-auto py-10 px-24">
       <CreateLead />
