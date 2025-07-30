ed01083700ee3255d1617d0fa6e4ef4e30150cf7
Kenna <kennataddese6@gmail.com>
2025-07-30 16:32:37 +0300

    feat: add status enum and field to leads table schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,4 @@
-import { integer, pgTable, timestamp, varchar } from "drizzle-orm/pg-core"
+import { integer, pgEnum, pgTable, timestamp, varchar } from "drizzle-orm/pg-core"
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
@@ -7,10 +7,20 @@ export const usersTable = pgTable("users", {
   password: varchar({ length: 255 }).notNull(),
 })
 
+export const leadsStatusEnum = pgEnum("status", [
+  "new",
+  "sent",
+  "followed_up",
+  "failed",
+  "deleted",
+  "converted",
+])
+
 export const leadsTable = pgTable("leads", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   email: varchar({ length: 255 }).notNull().unique(),
   description: varchar({ length: 255 }).notNull(),
+  status: leadsStatusEnum("status").default("new"),
   createdAt: timestamp({ mode: "date" }).defaultNow().notNull(),
   updatedAt: timestamp({ mode: "date" }).defaultNow().notNull(),
 })
